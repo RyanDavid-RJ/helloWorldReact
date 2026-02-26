780b7e9c626a1b5a50f89f8ffc485c23155d4fe3
Ryan David Riberio Dutra <cru0168995@aluno.firjansenaisesi.com.br>
2026-02-26 16:20:49 -0300

    Commit Final 23/02/2025

--- src/componentes/home.jsx
+++ src/componentes/home.jsx
@@ -1,15 +1,42 @@
 import '../componentes/home.css'
 
+function Home() {
 
-function Home(){
+    const noticias = [
+        {
+            id: 1,
+            imagem: "https://loremflickr.com/400/300/technology",
+            titulo: "Nova IA revoluciona o mercado",
+            descricao: "Uma nova tecnologia promete mudar a forma como usamos inteligência artificial."
+        },
+        {
+            id: 2,
+            imagem: "https://loremflickr.com/400/300/programming",
+            titulo: "React domina o mercado",
+            descricao: "React continua sendo uma das bibliotecas mais usadas do mundo."
+        },
+        {
+            id: 3,
+            imagem: "https://loremflickr.com/400/700/cybersecurity",
+            titulo: "Segurança digital em alta",
+            descricao: "Ataques cibernéticos aumentam e exigem mais proteção."
+        }
+    ];
 
-    return(
-        <>
-        <div  className="hr">
-        <h1>Home</h1>
+    return (
+        <div className="container">
+
+            <div className="cards">
+                {noticias.map(noticia => (
+                    <div alt ={noticia.id}>
+                        <img src={noticia.imagem} alt={noticia.titulo} className='cardImg' />
+                        <h2 className='cardTitle'>{noticia.titulo}</h2>
+                        <p className='cardDscr'>{noticia.descricao}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      
-        </>
     )
 }
-export default Home
+
+export default Home;
--- src/componentes/menu.jsx
+++ src/componentes/menu.jsx
@@ -1,11 +1,21 @@
-import './Menu.css'
+import './menu.css'
 
-function Menu(){
+function Menu() {
 
-    return(
-        <>
-        <h1>Menu</h1>
-        </>
-    )
+    return (
+
+        <nav className="menu">
+            <h1>Portal Noticias</h1>
+            <ul>
+                <li>Home</li>
+                <li>Tecnologia</li>
+                <li>Contatos</li>
+            </ul>
+
+        </nav>
+
+    );
 }
-export default Menu
+
+export default Menu;
+
